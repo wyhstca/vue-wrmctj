6312c9b37401c514bd92ce24e91d6fa84c3c6e66
wyhstca <wyhstca@outlook.com>
2022-02-23 17:02:37 +0800

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App " />
   </div>
 </template>
 
